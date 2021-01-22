94d8a72580cbad415c61efd782fff37dc5313383
MinHyeokBae <bmh9175@gmail.com>
2021-01-23 01:55:46 +0900

    조건부렌더링

--- src/Hello.js
+++ src/Hello.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-function Hello({ color, name }) {
+function Hello({ color, name, isSpecial }) {
   return (
     <div
       style={{
         color,
       }}
     >
-      안녕하세요 {name}
+      {isSpecial ? <b>*</b> : null} // 선택사항이 존재 할 때 3항 연산자 //{" "}
+      {isSpecial && <b>*</b>} 보여주고 숨기고 && 안녕하세요 {name}
     </div>
   );
 }
