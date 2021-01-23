8224210bcccda63b6193ac1e12ca25d16a57cb39
MinHyeokBae <bmh9175@gmail.com>
2021-01-23 21:47:07 +0900

    배열에서 항목 삭제하기
    array.filter 사용하기

--- src/UserList.js
+++ src/UserList.js
@@ -1,23 +1,26 @@
 import React from "react";
 
-function User({ user }) {
-  const { username, email } = user;
+function User({ user, onRemove }) {
+  const { id, username, email } = user;
   return (
     <div>
       <b>{username}: </b>
       <span>{email}</span>
+      <button onClick={() => onRemove(id)}>삭제</button>
     </div>
   );
 }
 
-function UserList({ users }) {
+function UserList({ users, onRemove }) {
   return (
     <div>
       {users.map((user) => (
-        <User user={user} key={user.id} />
+        <User user={user} key={user.id} onRemove={onRemove} />
       ))}
     </div>
   );
 }
 
 export default UserList;
+/*onClick={onRemove(id)}>삭제 이렇게 하면 렌더링 되는 시점에 onRemove가
+  동작한다.*/
