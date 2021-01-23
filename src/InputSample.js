d90695e098349c33c80556a7d07557cc8e0344d0
MinHyeokBae <bmh9175@gmail.com>
2021-01-23 11:45:44 +0900

    여러개의 inputs 다루기
    객체를 다룰때에는 꼭 복사(spread문법을 사용)를 하고 값을 바꿔야한다.

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,21 +1,42 @@
 import React, { useState } from "react";
 
 function InputSample() {
-  const [text, setText] = useState("");
+  const [inputs, setInputs] = useState({
+    name: "",
+    nickname: "",
+  });
+  const { name, nickname } = inputs;
   const onChange = (e) => {
-    console.log(e.target); //e의 타겟 DOM
+    const { name, value } = e.target;
+    console.log(e.target.name);
     console.log(e.target.value);
-    setText(e.target.value);
+
+    const nextInputs = {
+      //객체를 업데이트 시키는 방법
+      ...inputs,
+      [name]: value, //name의 값에 따라 다른 key를 가리키게 된다
+    };
+    setInputs(nextInputs);
   };
   const onReset = () => {
-    setText("");
+    setInputs({
+      name: "",
+      nickname: "",
+    });
   };
   return (
     <div>
-      <input onChange={onChange} value={text} />
+      <input name="name" placeholder="이름" onChonge={onChange} value={name} />
+      <input
+        name="nickname"
+        placeholder="닉네임"
+        onChange={onChange}
+        value={nickname}
+      />
       <button onClick={onReset}>초기화</button>
       <div>
-        <b>값: {text}</b>
+        <b>값: </b>
+        {name}: {{ nickname }}
       </div>
     </div>
   );
