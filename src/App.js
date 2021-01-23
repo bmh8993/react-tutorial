7539fb2d841f4cb8156205adbba19e45a393864b
MinHyeokBae <bmh9175@gmail.com>
2021-01-23 21:20:12 +0900

    배열에 항목 추가하기
    - 배열에도 spread 를 사용할 수 있다.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+
+  const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: "velopert",
@@ -21,15 +35,36 @@ function App() {
       email: "public.velopert3@gmail.com",
       active: false,
     },
-  ];
+  ]);
 
   const nextId = useRef(4);
 
   const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers(users.concat(user));
+    // setUsers([...users, user]); 위와 동일
+    setInputs({
+      username: "",
+      email: "",
+    });
     nextId.current += 1;
   };
 
-  return <UserList users={users} />;
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />;
+    </>
+  );
 }
 
 export default App;
