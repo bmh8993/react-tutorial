bec4559207e7767f2829872b360e6ed3350a46fc
MinHyeokBae <bmh9175@gmail.com>
2021-01-24 12:54:24 +0900

    useEffect

--- src/UserList.js
+++ src/UserList.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function User({ user, onRemove, onToggle }) {
   const { id, username, email, active } = user;
+  //useEffect(() => {
+  //  console.log("컴포넌트가 화면에 나타남");
+  //  // props -> state로 설정
+  //  // REST API
+  //  // setInterval, setTimeout
+  //  // UI가 화면에 나타난 이후임
+  //  return () => {
+  //    // clearInterval, clearTimeout
+  //    console.log("컴포넌트가 화면에서 사라짐");
+  //  };
+  //}, []);
+  // deps가 비어있으면 mount, unmount 될 때만 동작
+  // useEffect(() => {
+  //   console.log("user 값이 설정됨");
+  //   console.log(user);
+  //   return () => {
+  //     console.log("user 값이 바뀌기 전");
+  //     console.log(user);
+  //   };
+  //   // deps에 설정된 값이
+  //   // 해당 값이 바뀔 때/생성 될 때 마다 호출,
+  //   // 해당 값이 바뀌기 전은 return 부분을 진행
+  // }, [user]); // user에 deps가 걸려있으므로 user가 생성되거나 변경될 때 동작함
+  // useEffect(() => {
+  //   console.log(user);
+  // });
+  // // deps를 생략하면
+  // // 모든 컴포넌트에서 useEffect가 실행됨
+  // // 부모컴포넌트가 리렌더 되면 자식컴포넌트가 리렌더 
   return (
     <div>
       <b
